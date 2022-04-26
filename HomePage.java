ab61f5013865c84acb55af7d120526314c4cc0ab
Raissa Moraes <raissa_m_@hotmail.com>
2022-04-25 21:42:24 -0300

    Caso, plano e desenho de testes

---
+++ HomePage.java
@@ -0,0 +1,48 @@
+package testeSenai;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void Setup() {
+		System.setProperty("webdriver.chrome.driver", "D:\\JavaJDK\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();	
+		
+		
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		
+		
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void TesteNavegarGoogle(){
+		
+		
+		driver.get("https://online.sp.senai.br/");
+		
+		WebElement input = driver.findElement(By.id("Busca1_txtFiltro"));
+				
+		input.sendKeys("gestão");
+		//input.sendKeys(Keys.ENTER);
+		
+		driver.findElement(By.id("Busca1_btnBusca")).click();
+	}
+	
+	/*@After
+	public void FecharNavegador() {
+		driver.close();
+	}*/
+	
+}
